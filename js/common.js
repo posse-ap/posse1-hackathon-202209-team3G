28a684ea4eaf18d693bbf57070b3ca2ffc53d9be
fumiyashimotori <0et52x402pj189m@au.com.co.jp>
2022-09-04 18:40:01 +0900

    制作途中

--- js/common.js
+++ js/common.js
@@ -3,3 +3,37 @@
 // miu
 
 // fumiya
+  const all_profile = [
+    {
+      id: 1,
+      
+      nick_name: "さきの",
+      birthday: "3月28日",
+      
+    },
+    {
+      id: 2,
+      nick_name: "みう",
+      birthday: "11月29日",
+
+    },
+    {
+      id: 3,
+      nick_name: "ふみや",
+      birthday: "1月24日",
+
+    }
+  ];
+
+
+      // <div class="set">
+      //   <div id="profile">
+      //     <div id="pro_img"></div>
+      //     <div id="pro_name"></div>
+      //     <div id="pro_date"></div>
+      //   </div>
+      //   <div id="card_message">
+      //     <div ></div>
+      //     <div></div>
+      //   </div>
+      // </div>
