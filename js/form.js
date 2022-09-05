cd24239e3fc9747c30b19d571170f5c8e705504d
Sakino Miki <sakinom328@gmail.com>
2022-09-05 17:10:15 +0900

    入力し、保存したものを表示できるしたものを表示できるよものを表示でき るようにした

--- js/form.js
+++ js/form.js
@@ -1,51 +1,47 @@
 'use strict';
 
 /*入力フォームの内容を保存*/
-// {
-  // function buttonClick(event){
-  //   msg.innerText = 'お名前は' + content.value + 'さんですね';
-  // }
-  
-  // let checkButton = document.getElementById('checkButton');
-  // checkButton.addEventListener('click', buttonClick);
-  
-  // let title = document.getAttribute('data-title');
-  // title.addEventListener('change', inputChange);
-  // let name = document.getAttribute('data-name');
-  // name.addEventListener('change', inputChange);
-  // let content = document.getElementById('content');
-  // content.value = 'なまえ';
-  // let msg = document.getElementById('msg');
-  // function buttonClick(){
-  //   msg.innerText = 'お名前は' + nameText.value + 'さんですね';
-  // }
-  
-  // let nameText = document.getElementById('nameText');
-  // nameText.value = '名前';
-  // let msg = document.getElementById('msg');
-  
-  // let checkButton = document.getElementById('checkButton');
-  // checkButton.addEventListener('click', buttonClick);
-  
-  // }
-{
-  const showMessage = () => {
-    const title = document.getElementById("titles").value;
-    document.getElementById("output-title").textContent = title;
 
-    const name = document.getElementById("names").value;
-    document.getElementById("output-name").textContent = name;
+let speeches = [];
+const info = document.getElementById('info');
+let num = 0;
 
-    const textbox = document.getElementById("input-message");
-    const inputValue = textbox.value;
+const showMessage = () => {
+  num += 1;
+  num += Number(number);
+  number = 0;
 
-    //テキストボックスの値を使って、出力するメッセージを生成する
-    const outputMessage = inputValue;
-    //出力用のp要素にメッセージを表示
-    document.getElementById("output-message").innerHTML = outputMessage;
+  const title = document.getElementById("titles").value;
+  document.getElementById("output-title").textContent = title;
+  
+  const name = document.getElementById("names").value;
+  document.getElementById("output-name").textContent = name;
+  
+  const textbox = document.getElementById("input-message");
+  const inputValue = textbox.value;
+  
+  //テキストボックスの値を使って、出力するメッセージを生成する
+  const outputMessage = inputValue;
+  //出力用のp要素にメッセージを表示
+  document.getElementById("output-message").innerHTML = outputMessage;
 
-    localStorage.setItem('save', title, name, outputMessage);
-  }
-  const value4 = localStorage.getItem('save4');
-  counter4.innerHTML = value4;
+  const createHtml = 
+    `<div class="speech">
+    <p id="output-title${num}">${title}</p>
+    <p id="output-name${num}">${name}</p>
+    <p id="output-message${num}">${outputMessage}</p>
+    </div>`;
+    
+  speeches.push( createHtml );
+
+  localStorage.setItem('num', num);
+  localStorage.setItem('array', speeches);
 }
+
+let number = localStorage.getItem('num');
+
+const information = localStorage.getItem('array');
+speeches.push(information);
+
+info.innerHTML = speeches;
+  // localStorage.clear();
